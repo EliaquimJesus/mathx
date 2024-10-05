bce775e83df924d49f6869831e11413b3bac0018
Eliaquim Luz <eliaquim.daluzyahoo.com>
2024-10-05 23:24:27 +0100

    004

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -15,7 +15,20 @@ class MainController extends Controller
 
     public function generateExercises(Request $request): void
     {
-        echo 'gerar exercícios';
+        // form validation
+        $request->validate([
+            'check_sum' => 'required_without_all:check_subtraction,check_multiplication,check_division',
+            'check_subtraction' => 'required_without_all:check_sum,check_multiplication,check_division',
+            'check_multiplication' => 'required_without_all:check_subtraction,check_sum,check_division',
+            'check_division' => 'required_without_all:check_subtraction,check_multiplication,check_sum',
+            'number_one' => 'required|integer|min:0|max:999|lt:number_two',
+            'number_two' => 'required|integer|min:0|max:999',
+            'number_exercises' => 'required|integer|min:5|max:50',
+        ]);
+
+        
+
+        //dd($request->all());
     }
 
     public function printExercises(): void
