ebf093dfdf730728b3efc8fcbf081a226ebdb789
Eliaquim Luz <eliaquim.daluzyahoo.com>
2024-10-06 01:00:40 +0100

    006

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -13,7 +13,7 @@ class MainController extends Controller
         return view('home');
     }
 
-    public function generateExercises(Request $request): void
+    public function generateExercises(Request $request): View
     {
         // form validation 
         $request->validate([
@@ -83,7 +83,7 @@ class MainController extends Controller
             ];
         }
 
-        dd($exercises);
+        return view('operations', ['exercises' => $exercises]);
 
     }
 
